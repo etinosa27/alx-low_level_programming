f3d8c2f08131ef4ec2b97cda4ef91da070be3921
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-07-01 00:17:13 +0000

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints quote to the standard error
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 
 	return (1);
 }
