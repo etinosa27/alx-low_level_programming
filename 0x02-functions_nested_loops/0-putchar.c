ba0e78859a981b09815f315df141fa3ca4387136
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-07-05 07:39:14 -0700

    0 _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,17 @@
-#include main.h
-printf("_putchar\n");
+#include "main.h"
+/**
+ * main - print Holberton, followed by new line
+ * Return: 0
+ */
+int main void {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
