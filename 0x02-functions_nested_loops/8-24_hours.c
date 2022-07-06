73b3ad55d1017414f37dba33d2c38014dabeeea8
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-07-06 00:56:21 +0000

    I am federal agent

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack bauer - prints all minutes of the day Jack Bauer
+ * Return: 0
+ */
+
+void jack_buer(void)
+		{
+		
+		int h1;
+		int h2;
+		int m1;
+		int m2;
+		int a = 9;
+
+		h2 = 0;
+		while (h2 <= 2)
+		{
+		if (h2==2)
+		{
+		/* Restrain to 23h, not 29*/
+		a = 3;
+		}
+		h1 = 0;
+		while (h1 <= a)
+		{
+		m2 = 0;
+		while  (m2 <= 5)
+		{
+			m1 = 0;
+			while (m1 <= 9)
+			{
+				_purchar('0' + h2);
+				_purchar('0' + h1);
+				_purchar(':');
+				_purchar('0' + m2);
+				_purchar('0' + m1);
+				_purchar('\n');
+				m1++;
+			}
+			m2++;
+		}
+		h1++;
+		}
+		h2++;
+		}
+		}
