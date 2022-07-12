d795669ef3a708b5d4ed4616a475adbe47a19974
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-07-12 20:19:08 +0000

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,33 @@
 #include "main.h"
 
 /**
- * puts2 - prints characters in a string
- * in steps of 2
- * @str: Array of characters
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
  */
 void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	while (*(str + i) != 0)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
-	_putchar(10);
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,44 @@
 #include "main.h"
 
 /**
- * puts_half - Prints out the first half of a string.
- * @str: input string to print.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+	}
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
  */
 
 void puts_half(char *str)
 {
-	int i, len, m;
+	int idx;
+	int len = _strlen(str);
 
-	for (len = 0; str[len] != '\0'; len++)
-	{
-	}
-	if ((len % 2) == 0)
-		m = (len / 2);
+	/* find the index to start depending on even/odd amount of strlen */
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
 	else
-		m = (len + 1) / 2;
+		idx = (len / 2);
 
-	for (i = m; i < len; i++)
+	while (idx < len)
 	{
-		_putchar(str[i]);
+		_putchar(*(str + idx));
+		idx++;
 	}
-	_putchar('\n');
+	-putchar('\n');
 }
