aac35b55c4494958ea4fdd53f18606985fa593eb
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-07-28 23:01:10 +0000

    realloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -9,6 +12,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
-
 
 #endif
