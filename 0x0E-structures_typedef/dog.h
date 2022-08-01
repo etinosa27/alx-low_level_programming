9ce9762012eac470777cf2895181b7a462e1524a
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-08-01 16:26:47 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,6 @@ struct dog
 	char *owner;
 };
 
-int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
