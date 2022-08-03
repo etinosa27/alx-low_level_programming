87fe525d11f0c05686d12d7b6779ba7073956166
Etinosa Igbinevbo <etinosa27@gmail.com>
2022-08-03 18:48:22 +0000

    Most hackers are young because young people tend to be adaptable.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS
-#define _FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 
 /*
  * Desc: Header file containing prototypes for all functions
@@ -11,4 +11,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /*_FUNCTION_POINTERS_H*/
